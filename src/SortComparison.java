a135f17114b8f77451179b26ab390b833dbac2a6
Sophie Crowley <sophiecrowley98@gmail.com>
2019-02-24 19:02:04 +0000

    Completed Quicksort

--- src/SortComparison.java
+++ src/SortComparison.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 // -------------------------------------------------------------------------
@@ -11,149 +14,217 @@ import java.util.concurrent.ThreadLocalRandom;
  *  @version HT 2019
  */
 
- class SortComparison {
+class SortComparison {
 
-    /**
-     * Sorts an array of doubles using InsertionSort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order.
-     *
-     */
-    static double [] insertionSort (double a[]){
-    	double temp;
-    	for(int i=1; i < a.length; i++)
-    	{
-    		for(int j = i;j>0;j--)
-    		{
-        		if(a[j] < a[j-1])
-        		{
-        			temp = a[j-1];
-        			a[j-1]=a[j];
-        			a[j]=temp;
-        		}
-    		}
-    		
-    	}
-		return a;
-    }
-    //end insertionsort
+	/**
+	 * Sorts an array of doubles using InsertionSort.
+	 * This method is static, thus it can be called as SortComparison.sort(a)
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order.
+	 *
+	 */
+	static double [] insertionSort (double a[]){
+		double temp;
+		for(int i=1; i < a.length; i++)
+		{
+			for(int j = i;j>0;j--)
+			{
+				if(a[j] < a[j-1])
+				{
+					temp = a[j-1];
+					a[j-1]=a[j];
+					a[j]=temp;
+				}
+			}
 
-    /**
-     * Sorts an array of doubles using Quick Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order
-     *
-     */
-    static double [] quickSort (double a[]){
-    	if(a.length <=1)
-    	{
-    		return a;
-    	}
-    	else
-    	{
-        	//Randomising array first
-        	Random num = ThreadLocalRandom.current();
-            for (int i = a.length - 1; i > 0; i--)
-            {
-              int index = num.nextInt(i + 1);
-              double current = a[index];
-              a[index] = a[i];
-              a[i] = current;
-            }
-            //now the actual sorting
-            a = quicksort(a, a[0], a[a.length-1]);
-    		return a;
-    	}
-    }
-    
-    
-    private static double[] quicksort(double[] a, double low, double high) {
-    	if(low < high)
-    	{
-    		
-    	}
+		}
 		return a;
 	}
+	//end insertionsort
+
+	/**
+	 * Sorts an array of doubles using Quick Sort.
+	 * This method is static, thus it can be called as SortComparison.sort(a)
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order
+	 *
+	 */
+	static double [] quickSort (double a[]){
+		if(a.length <=1)
+		{
+			return a;
+		}
+		else
+		{
+			//Randomising array first
+			Random num = ThreadLocalRandom.current();
+			for (int i = a.length - 1; i > 0; i--)
+			{
+				int index = num.nextInt(i + 1);
+				double current = a[index];
+				a[index] = a[i];
+				a[i] = current;
+			}
+			//now the actual sorting
+			 
+			quickSort(a, 0, a.length-1);
+			return a;
+		}
+	}
+
+
+	private static void quickSort(double[] a, int low, int high) {
+		if(high <= low) {
+			return;
+		}
+			int pivot = partition(a, low, high);
+			quickSort(a, low, pivot-1);
+			quickSort(a, pivot+1, high);
+	}
+	
+	private static int partition(double[] array, int low, int high) {
+		int i = low;
+		int j = (high+1);
+		double pivot = array[low];
+		while(true) {
+		while((array[++i]< pivot)) {
+		if(i == high) break;
+		}
+		while(pivot < array[--j]) {
+		if(j == low) break;
+		}
+		if(i >= j) break;
+		double temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		}
+		array[low] = array[j];
+		array[j] = pivot;
+		return j;
+		}
 	//end quicksort
 
-    /**
-     * Sorts an array of doubles using Merge Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order
-     *
-     */
-    /**
-     * Sorts an array of doubles using iterative implementation of Merge Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     *
-     * @param a: An unsorted array of doubles.
-     * @return after the method returns, the array must be in ascending sorted order.
-     */
+	/**
+	 * Sorts an array of doubles using Merge Sort.
+	 * This method is static, thus it can be called as SortComparison.sort(a)
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order
+	 *
+	 */
+	/**
+	 * Sorts an array of doubles using iterative implementation of Merge Sort.
+	 * This method is static, thus it can be called as SortComparison.sort(a)
+	 *
+	 * @param a: An unsorted array of doubles.
+	 * @return after the method returns, the array must be in ascending sorted order.
+	 */
 
-    static double[] mergeSortIterative (double a[]) {
+	static double[] mergeSortIterative (double a[]) {
+		double aux[] = new double[a.length];
+		for(int i=1; i < a.length;i+=i)
+		{
+			for(int low = 0; low < a.length-i; low +=i+i)
+			{
+				merge(a,aux,low,(low+i)-1,Math.min(low+i+i-1, a.length-1));
+			}
+		}
 		return a;
 
-		 //todo: implement the sort
+	}
+
+	//end mergesortIterative
+
+
+
+	/**
+	 * Sorts an array of doubles using recursive implementation of Merge Sort.
+	 * This method is static, thus it can be called as SortComparison.sort(a)
+	 *
+	 * @param a: An unsorted array of doubles.
+	 * @return after the method returns, the array must be in ascending sorted order.
+	 */
+	static double[] mergeSortRecursive (double a[]) {
+		double[] aux = new double[a.length];
+		mergeSort(a,aux,0,a.length-1);
+		return a;
+
+	}
 	
-    }
-    //end mergesortIterative
-    
-    
-    
-    /**
-     * Sorts an array of doubles using recursive implementation of Merge Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     *
-     * @param a: An unsorted array of doubles.
-     * @return after the method returns, the array must be in ascending sorted order.
-     */
-    static double[] mergeSortRecursive (double a[]) {
-		return a;
-    	
-
-    	//todo: implement the sort
+	private static void mergeSort (double[] a, double[] aux, int low, int high)
+	{
+		if(high <= low)
+		{
+			return;
+		}
+		int mid = low + ((high-low) /2);
+		mergeSort(a,aux,low,mid);
+		mergeSort(a,aux,mid+1,high);
+		merge(a,aux,low,mid,high);
+	}
 	
-   }
-    //end mergeSortRecursive
-    	
-    
-    /**
-     * Sorts an array of doubles using Selection Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order
-     *
-     */
-    static double [] selectionSort (double a[]){
-    	for(int i=0;i<a.length-1;i++)
-    	{
-    		int temp = i ;
-    		for(int j=i+1;j<a.length;j++)
-    		{
-    			if(a[j] < a[temp])
-    			{
-    				temp = j;
-    			}
-    		}
-    		double temporary = a[temp];
-    		a[temp] = a[i];
-    		a[i] = temporary;
-    	}
+	private static void merge (double[] a, double[] aux, int low, int mid, int high)
+	{
+		for(int i= low;i<=high; i++)
+		{
+			aux[i] = a[i];
+		}
+		int j = low;
+		int k = mid+1;
+		for(int n = low; n <=high;n++)
+		{
+			if(j > mid)
+			{
+				a[n] = aux[k++];
+			}
+			else if(k > high)
+			{
+				a[n] = aux[j++];
+			}
+			else if(aux[k] < aux[j])
+			{
+				a[n] = aux[k++];
+			}
+			else
+			{
+				a[n] = aux[j++];
+			}
+		}
+	}
+	//end mergeSortRecursive
+
+
+	/**
+	 * Sorts an array of doubles using Selection Sort.
+	 * This method is static, thus it can be called as SortComparison.sort(a)
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order
+	 *
+	 */
+	static double [] selectionSort (double a[]){
+		for(int i=0;i<a.length-1;i++)
+		{
+			int temp = i ;
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[j] < a[temp])
+				{
+					temp = j;
+				}
+			}
+			double temporary = a[temp];
+			a[temp] = a[i];
+			a[i] = temporary;
+		}
 		return a;
-    }
-    //end selectionsort
-
-   
+	}
+	//end selectionsort
 
 
-    public static void main(String[] args) {
 
-        //todo: do experiments as per assignment instructions
-    }
 
- }
- //end class
+	public static void main(String[] args) throws FileNotFoundException {
+	}
+}
+
+//end class
 
--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -8,7 +12,7 @@ import org.junit.runners.JUnit4;
 /**
  *  Test class for SortComparison.java
  *
- *  @author
+ *  @author Sophie Crowley
  *  @version HT 2019
  */
 @RunWith(JUnit4.class)
@@ -33,19 +37,118 @@ public class SortComparisonTest
     }
 
 
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+    // TODO: add more tests here. Each line of code and each decision in Collinear.java should
     // be executed at least once from at least one test.
 
     // ----------------------------------------------------------
     /**
      *  Main Method.
      *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     * @throws FileNotFoundException 
      *
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
-        //TODO: implement this method
-    }
+		double[] array = null;
+		Scanner inFile = null;
+		for(int y=0; y<7;y++)
+		{ 
+			if(y==0)
+			{
+				inFile = new Scanner(new File("numbers10.txt"));
+				array = new double[10];
+			}
+			else if(y==1)
+			{
+				inFile = new Scanner(new File("numbers100.txt"));
+				array = new double[100];
+			}
+			else if(y==2)
+			{
+				inFile = new Scanner(new File("numbers1000.txt"));
+				array = new double[1000];
+			}
+			else if(y==3)
+			{
+				inFile = new Scanner(new File("numbers1000Duplicates.txt"));
+				array = new double[1000];
+			}
+			else if(y==4)
+			{
+				inFile = new Scanner(new File("numbersNearlyOrdered1000.txt"));
+				array = new double[1000];
+			}
+			else if(y==5)
+			{
+				inFile = new Scanner(new File("numbersReverse1000.txt"));
+				array = new double[1000];
+			}
+			else if(y==6)
+			{
+				inFile = new Scanner(new File("numbersSorted1000.txt"));
+				array = new double[1000];
+			}
+			int x=0;
+			while(inFile.hasNextDouble())
+			{
+				array[x] = inFile.nextDouble();
+				x++;
+			}
+			double currentArray[] = null;
+			System.out.println("\nUsing test case " + (y+1) + "\n");
+			System.out.println("Selection sort");
+			for(int j=0;j<4;j++)
+			{
+				currentArray = array.clone();
+				long startTime = System.nanoTime();
+				SortComparison.selectionSort(currentArray);
+				long endTime = System.nanoTime();
+				long timeElapsed = endTime - startTime;
+				System.out.println("Execution time in nanoseconds is:" + timeElapsed);
+			}
+			System.out.println("\nInsertion sort");
+			for(int j=0;j<4;j++)
+			{
+				currentArray = array.clone();
+				long startTime = System.nanoTime();
+				SortComparison.insertionSort(currentArray);
+				long endTime = System.nanoTime();
+				long timeElapsed = endTime - startTime;
+				System.out.println("Execution time in nanoseconds is:" + timeElapsed);
+			}
+			System.out.println("\nQuick sort");
+			for(int j=0;j<4;j++)
+			{
+				currentArray = array.clone();
+				long startTime = System.nanoTime();
+				SortComparison.quickSort(currentArray);
+				long endTime = System.nanoTime();
+				long timeElapsed = endTime - startTime;
+				System.out.println("Execution time in nanoseconds is:" + timeElapsed);
+			}
+			System.out.println("\nRecursive Merge sort");
+			for(int j=0;j<4;j++)
+			{
+				currentArray = array.clone();
+				long startTime = System.nanoTime();
+				SortComparison.mergeSortRecursive(currentArray);
+				long endTime = System.nanoTime();
+				long timeElapsed = endTime - startTime;
+				System.out.println("Execution time in nanoseconds is:" + timeElapsed);
+			}
+			System.out.println("\nIterative Merge sort");
+			for(int j=0;j<4;j++)
+			{
+				currentArray = array.clone();
+				long startTime = System.nanoTime();
+				SortComparison.mergeSortIterative(currentArray);
+				long endTime = System.nanoTime();
+				long timeElapsed = endTime - startTime;
+				System.out.println("Execution time in nanoseconds is:" + timeElapsed);
+			}
+		}
+		inFile.close();
+	}
 
 }
 
